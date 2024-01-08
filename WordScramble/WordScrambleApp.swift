4823cc558a94687aa19e02974f9b50d5534eecb6
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-01-08 11:26:00 +1030

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Woodrow Martyr on 8/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
